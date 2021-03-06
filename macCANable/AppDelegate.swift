c5c8f3eb3e7e3fc9d081e764ed6da405dc18748f
Robert Huston <robert.huston@pinpointdynamics.com>
2021-03-06 13:35:41 -0500

    Initial Commit

---
+++ macCANable/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  macCANable
+//
+//  Created by Robert Huston on 3/6/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
